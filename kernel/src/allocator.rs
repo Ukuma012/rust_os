f69a125acfb2075ddc32d536d5bb59aedb4b87a5
Ukuma012 <ukuma012@gmail.com>
2024-09-15 12:42:56 +0900

    print memory info

--- kernel/src/allocator.rs
+++ kernel/src/allocator.rs
@@ -1,7 +1,7 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 
-use crate::memory_manager::{frame_manager, Frame};
+use crate::{memory_manager::{frame_manager, Frame}, println};
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -12,8 +12,13 @@ unsafe impl GlobalAlloc for MemoryAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let num_frames = (layout.size() + layout.align() + Frame::SIZE - 1) / Frame::SIZE;
         match  frame_manager().allocate(num_frames) {
-            Ok(frame) => (frame.frame_id() * Frame::SIZE + layout.align()) as *mut u8,
+            Ok(frame) =>{
+             let ptr = (frame.frame_id() * Frame::SIZE + layout.align()) as *mut u8;
+             println!("Allocated {:?} at {:p} (frame_id: {})", layout, ptr, frame.frame_id());
+             ptr
+            },
             Err(_) => {
+                println!("Failed to allocate {:?}", layout);
                 null_mut()
             }
         }
@@ -22,6 +27,7 @@ unsafe impl GlobalAlloc for MemoryAllocator {
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         let frame_start = Frame::new(ptr as usize / Frame::SIZE);
         let num_frames = (layout.size() + layout.align() + Frame::SIZE - 1) / Frame::SIZE;
+        println!("Deallocating {:?} at {:p} (frame_id: {})", layout, ptr, frame_start.frame_id());
         frame_manager().free(frame_start, num_frames)
     }
 }
