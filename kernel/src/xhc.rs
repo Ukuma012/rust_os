194f441e4310fe1fb8aa70603d25f02e3489195a
Ukuma012 <ukuma012@gmail.com>
2024-09-23 22:14:23 +0900

    delete a comment

--- kernel/src/xhc.rs
+++ kernel/src/xhc.rs
@@ -129,7 +129,6 @@ where
     fn on_event(&mut self, event_trb: EventTrb) {
         match event_trb {
             EventTrb::TransferEvent { transfer_event, target_event } => {
-                println!("{:?}", transfer_event);
                 self.on_transfer_event(transfer_event, target_event);
             }
             EventTrb::CommandCompletionEvent(completion) => {
